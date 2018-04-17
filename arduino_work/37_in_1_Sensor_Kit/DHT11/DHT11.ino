940fd7c3dce9d48c07732446ba6171e2aa8cc8aa
Danny <38252693+ErebusC@users.noreply.github.com>
2018-04-17 10:05:41 +0100

    Made small amendments
    Made a few small amendments to the comments and the contents of the code.

--- arduino_work/37_in_1_Sensor_Kit/DHT11/DHT11.ino
+++ arduino_work/37_in_1_Sensor_Kit/DHT11/DHT11.ino
@@ -4,32 +4,24 @@ dht DHT;
 // deines the pin to the digital input its connected into.
 #define DHT11_PIN 2
 
-//int pin = 2;
-
 void setup() {
-  //pinMode(pin, INPUT);
-
-  //Tells the arduino what serial monitor to print to.
+   //Tells the arduino what serial monitor to print to.
   Serial.begin(9600);
-  
-
-}
+  }
 
 void loop() {
-  /* My attempt to check to make sure the pin was accepting data.
-  value = digitalRead(pin);
-  Serial.println(value);
-  */
-
   /*Initialises the DHT and allows for you to read the data from the sensor
    * In the library DHT.read11 checks that the DHT is functional.
    * This library works with multiple types of DHT sensors, not just the 11 model.
    */
   int check = DHT.read11(DHT11_PIN);
-
+  
+  //Prints the below text with the temperature data
   Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
+  Serial.println(DHT.temperature); //println creates an escape character, effectively creating a new line in the serial monitor
+  
+  //Prints the below text with the humidity data
   Serial.print("Humidity = ");
   Serial.println(DHT.humidity);
-  delay(1000);
+  delay(5000); //5 second delay
 }
