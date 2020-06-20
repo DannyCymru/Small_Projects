66270e6f5479a2065c5f45b5c6330c0d30490604
Danny <danny@erebus.cymru>
2020-06-20 13:00:54 +0100

    Added some comments

--- Py-Zip/py-zip.py
+++ Py-Zip/py-zip.py
@@ -1,21 +1,26 @@
-import glob
 import os
-from pathlib import Path
 import zipfile
 
+#Current directory
 dir_path = os.getcwd()
 
+#root provides all path information, files returns actual file name + extension
 for root, dirs, files in os.walk(dir_path):
     for name in files:
+        #Combines the file name into the path information for a full file location
         path = root + '/' + name
 
         if name.endswith('.zip'):
             zip_location = os.path.abspath(path)
             zip_ref = zipfile.ZipFile(zip_location, 'r')
+            # OS path splits the name away from the file extension and path variable
             new_folder = os.path.splitext(zip_location)[0][0:]
+            # Creates a new folder in the exact name of the original zip
             os.mkdir(new_folder)
+            #extracts all zip contents to the new folder
             zip_ref.extractall(new_folder)
             zip_ref.close()
+            #Removes original zip
             os.remove(zip_location)
 
         elif name.endswith('.cbr'):
