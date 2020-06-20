b80955bd21fceac9b03d04cef55f53685e140b16
Danny <danny@erebus.cymru>
2020-06-20 16:36:57 +0100

    Fixed bug where if it found any folders/extensions not covered it would instantly stop

--- unzipr/unzipr.py
+++ unzipr/unzipr.py
@@ -1,15 +1,15 @@
 import os
 import zipfile
 
-#Current directory
+# Current directory
 dir_path = os.getcwd()
 
-#root provides all path information, files returns actual file name + extension
+# root provides all path information, files returns actual file name + extension
 for root, dirs, files in os.walk(dir_path):
     for name in files:
-        #Combines the file name into the path information for a full file location
+        # Combines the file name into the path information for a full file location
         path = root + '/' + name
-
+        print(path)
         if name.endswith('.zip'):
             zip_location = os.path.abspath(path)
             zip_ref = zipfile.ZipFile(zip_location, 'r')
@@ -17,10 +17,10 @@ for root, dirs, files in os.walk(dir_path):
             new_folder = os.path.splitext(zip_location)[0][0:]
             # Creates a new folder in the exact name of the original zip
             os.mkdir(new_folder)
-            #extracts all zip contents to the new folder
+            # extracts all zip contents to the new folder
             zip_ref.extractall(new_folder)
             zip_ref.close()
-            #Removes original zip
+            # Removes original zip
             os.remove(zip_location)
 
         elif name.endswith('.cbr'):
@@ -37,4 +37,3 @@ for root, dirs, files in os.walk(dir_path):
 
         else:
             print("No folders found")
-            exit(0)
