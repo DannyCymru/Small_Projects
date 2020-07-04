be02b252982f0cb7cf3eb446b26a86621853f524
Danny <danny@erebus.cymru>
2020-07-04 11:31:22 +0100

    Improved script to extract Zip and Rar files in near identical format, to allow for little to no manual intervention

--- python_scripts/unzipr/unzipr.py
+++ python_scripts/unzipr/unzipr.py
@@ -1,39 +1,58 @@
 import os
 import zipfile
+import rarfile
+import errno
 
 # Current directory
 dir_path = os.getcwd()
 
+def zip(archive):
+    zip_ref = zipfile.ZipFile(archive, 'r')
+
+    zip_con = [zip_ref.namelist()]
+    for f in zip_con[0]:
+        if f.endswith('/'):
+            try:
+                #Extracts all and breaks the loop
+                zip_ref.extractall()
+                break
+            except(OSError, IOError) as err:
+                if err.errno != errno.EEXIST:
+                    raise
+        elif not f.endswith('/') and not os.path.isdir(new_folder):
+            try:
+                #Creates a new directory to place the extracted files
+                os.makedirs(new_folder)
+                zip_ref.extractall(new_folder)
+            except OSError as exc:
+                if exc.errno == errno.EEXIST and os.path.isdir(new_folder):
+                    pass
+
+def rar(archive):
+    rar_ref = rarfile.RarFile(archive, 'r')
+
+    rar_ref.extractall()
+
 # root provides all path information, files returns actual file name + extension
 for root, dirs, files in os.walk(dir_path):
     for name in files:
+
         # Combines the file name into the path information for a full file location
         path = root + '/' + name
-        print(path)
-        if name.endswith('.zip'):
-            zip_location = os.path.abspath(path)
-            zip_ref = zipfile.ZipFile(zip_location, 'r')
-            # OS path splits the name away from the file extension and path variable
-            new_folder = os.path.splitext(zip_location)[0][0:]
-            # Creates a new folder in the exact name of the original zip
-            os.mkdir(new_folder)
-            # extracts all zip contents to the new folder
-            zip_ref.extractall(new_folder)
-            zip_ref.close()
-            # Removes original zip
-            os.remove(zip_location)
 
-        elif name.endswith('.cbr'):
-            n_path = path.replace('.cbr', '.rar')
-            os.rename(path, n_path)
+        #Path to the archive
+        archive = os.path.abspath(path)
 
-        elif name.endswith('.CBR'):
-            n_path = path.replace('.CBR', '.rar')
-            os.rename(path, n_path)
+        # OS path splits the name away from the file extension and path variable
+        new_folder = os.path.splitext(archive)[0][0:]
 
-        elif name.endswith('.cbz'):
-            n_path = path.replace('.cbz', '.zip')
-            os.rename(path, n_path)
+        if name.endswith('.zip') or name.endswith('.cbz'):
+            zip(archive)
+            os.remove(archive)
 
+        elif name.endswith('.cbr') or name.endswith('.rar'):
+            rar(archive)
+            os.remove(archive)
+        
         else:
-            print("No folders found")
+            print(name, " is not a folder")
